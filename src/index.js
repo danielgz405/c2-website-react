4406e8f1d04d26d61ae2817907322c55b4686467
danielgz405 <danielfelipeg85@gmail.com>
2022-07-20 20:17:25 -0500

    [ADD] label ztitle and description

--- src/index.js
+++ src/index.js
@@ -14,6 +14,23 @@ import Store from "./pages/Store";
 import Admin from "./pages/Admin";
 import Error404 from "./pages/Error404";
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmV7Ya0cyFesqiAB8XTxIxJkcg6kiCwOg",
+  authDomain: "cyc-acabados-arquitectonicos.firebaseapp.com",
+  projectId: "cyc-acabados-arquitectonicos",
+  storageBucket: "cyc-acabados-arquitectonicos.appspot.com",
+  messagingSenderId: "26227357137",
+  appId: "1:26227357137:web:ab2e654ce6ade2a9b4dcf0",
+  measurementId: "G-YPXNC5K04M"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function App() {
   return (
     <BrowserRouter>
