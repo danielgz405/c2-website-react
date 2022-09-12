0ca6c823d76733bce644533d7502741c6d7893ad
danielgz405 <danielfelipeg85@gmail.com>
2022-09-12 12:02:27 -0500

    [ADD] form to cards and sections

--- src/components/admin/see/Home/index.jsx
+++ src/components/admin/see/Home/index.jsx
@@ -1,9 +1,11 @@
+import * as Heroicons from "@heroicons/react/outline";
 import InputList from "../../../../common/InputList";
 import Modal from "../../../../common/Modal";
 import { useEffect, useState } from "react";
-import { CameraIcon } from "@heroicons/react/outline";
+import { useRef } from "react";
 
 export default function Home({signOut, setAlert, auth, edit, setEdit}){
+  const formCards = useRef(null);
   const [open, setOpen] = useState(false);
   const items = [
     {value: 'cards', name: 'cards', id: 0},
@@ -16,6 +18,7 @@ export default function Home({signOut, setAlert, auth, edit, setEdit}){
     type: '',
     title: '',
     subTitle: '',
+    cards: [],
   });
   const logout = (event) => {
   event.preventDefault();
@@ -36,6 +39,27 @@ export default function Home({signOut, setAlert, auth, edit, setEdit}){
     });
   });
   };
+  const handleUploadCard = (e) => {
+    e.preventDefault();
+    const data = {
+      title: formCards.current.childNodes[0].value,
+      subTitle: formCards.current.childNodes[1].value,
+      icon: {src: Heroicons[formCards.current.childNodes[2].value], text: formCards.current.childNodes[2].value},
+      btnText: formCards.current.childNodes[3].value,
+    };
+    
+    setStructure({...structure, cards: [...structure.cards, data]});
+    console.log(structure.cards);
+  
+  };
+  const handleDeleteCards = (e, index) => {
+    e.preventDefault();
+
+    const card = structure.cards.filter((item, indx) => indx !== index);
+
+    setStructure({...structure, cards: card});
+
+  }
   useEffect(() => {
     setStructure({...structure, type: selected.value});
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -53,13 +77,57 @@ export default function Home({signOut, setAlert, auth, edit, setEdit}){
               <label className="title-table ml-1 mt-01" >tipo</label>
               <InputList items={items} selected={selected} setSelected={setSelected} />
               <label className="title-table ml-1" >Nombre de la seccion</label>
-              <input className="input-form" required onChange={(e) => (setStructure({...structure, title: e.target.value}))} value={structure.title} autoComplete="name" type="text" name="title" id="title" placeholder="Ingresa el nombre del producto" />
-              <label className="title-table ml-1 mt-01" >Descripcio nde la seccion</label>
-              <input className="input-form" required onChange={(e) => (setStructure({...structure, subTitle: e.target.value}))} value={structure.subTitle} autoComplete="name" type="text" name="title" id="title" placeholder="Ingresa el nombre del producto" />
+              <input className="input-form" required onChange={(e) => (setStructure({...structure, title: e.target.value}))} value={structure.title} autoComplete="name" type="text" name="title" id="title" placeholder="Nombre de la seccion" />
+              <label className="title-table ml-1 mt-01" >Descripcion de la seccion</label>
+              <input className="input-form" required onChange={(e) => (setStructure({...structure, subTitle: e.target.value}))} value={structure.subTitle} autoComplete="name" type="text" name="title" id="title" placeholder="Descripcion de la seccion" />
+
+              {selected.value === 'cards' && 
+              <>
+                 <label className="title-table ml-1 mt-01" >Cards</label>
+                    {structure.cards?.length > 0 && 
+                      <table>
+                        <thead>
+                          <tr>
+                            <th><label className="title-table ml-1">Title</label></th>
+                            <th><label className="title-table ml-1">SubTitulo</label></th>
+                            <th><label className="title-table ml-1">icon</label></th>
+                            <th><span className="title-table ml-1">delete</span></th>
+                          </tr>
+                        </thead>
+                        
+                        {structure.cards?.map((item, index) => (
+                          <tbody key={index}>
+                            <tr>
+                              <td className="table-galery text-secondarie"><p>{item.title}</p></td>
+                              <td className="table-galery text-secondarie"><p>{item.subTitle}</p></td>
+                              <td className="table-galery">
+                                <item.icon.src className="h-1"/>
+                              </td>
+                              <td className="table-galery">
+                                <div className="delete-buttom h-input" onClick={(e) => handleDeleteCards(e, index)}>
+                                  <Heroicons.TrashIcon className="h-1"/>
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        ))}
+                      </table>
+                    }
+
+                    <div ref={formCards} className="porfile-form">
+                      <input className="input-form" type="text" name="title" id="title" placeholder="Nombre de la card" />
+                      <input className="input-form mt-01" type="text" name="title" id="title" placeholder="Subtitulo de la card" />
+                      <input className="input-form mt-01" type="text" name="title" id="title" placeholder="Icono de la card" />
+                      <input className="input-form mt-01" type="text" name="title" id="title" placeholder="Texto del boton" />
+
+                      <input className="basic-buttom h-input mt-01" type="button" value="Agregar card" onClick={(e) => handleUploadCard(e)}/>
+                    </div>
+              </>}
             </div>
           </div>
           <div className="footer-section">
-            <input className="basic-buttom" type="submit" value="Guardar"/>
+            <div className="basic-buttom" >Info</div>
+            <input className="basic-buttom ml-1" type="submit" value="Guardar"/>
             <div className="basic-buttom ml-1" onClick={() => setOpen(!open)}>Cancelar</div>
           </div>
         </form>
